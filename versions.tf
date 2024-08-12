1f2fe882901e031f1674da0ba10fd3cf34376aa9
Jason Johnson <jasojohnson@microsoft.com>
2024-08-12 08:13:57 +0000

    we should not be so strict on provider reqs

--- versions.tf
+++ versions.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.80"
+      version = ">= 3.80"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 2.45.0"
+      version = ">= 2.45.0"
     }
     namep = {
       source  = "jason-johnson/namep"
-      version = "~> 1.1"
+      version = ">= 1.1"
     }
   }
 }
