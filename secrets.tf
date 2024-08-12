ece7de3c9b1018fd82f2671b6272d0b08fbd0fe4
Jason Johnson <jasojohnson@microsoft.com>
2024-08-12 07:49:48 +0000

    make secrets accessable by name not index

--- secrets.tf
+++ secrets.tf
@@ -1,8 +1,8 @@
 resource "azurerm_key_vault_secret" "main" {
-    count         = length(var.secrets)
-    name          = var.secrets[count.index].name
-    value         = var.secrets[count.index].value
-    key_vault_id  = azurerm_key_vault.main.id
+  for_each     = { for secret in var.secrets : secret.name => secret }
+  name         = each.value.name
+  value        = each.value.value
+  key_vault_id = azurerm_key_vault.main.id
 
-    depends_on = [ azurerm_key_vault_access_policy.manager, azurerm_role_assignment.manager, azurerm_role_assignment.manager_roles["secret_permissions"] ]
+  depends_on = [azurerm_key_vault_access_policy.manager, azurerm_role_assignment.manager, azurerm_role_assignment.manager_roles["secret_permissions"]]
 }
