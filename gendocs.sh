549613e04d3d0b00eb2cc947882cbfb7f616bdb3
Jason <jason.johnson.081@gmail.com>
2025-11-27 17:38:34 +0100

    Update version tag in gendocs.sh to v1.2.0 and fix regex

--- gendocs.sh
+++ gendocs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tag=v1.1.6
+tag=v1.2.0
 
-sed "s/.\/../github.com\/jason-johnson\/tf-azure-keyvault?ref=$tag/" examples/example.tf > examples/generated.txt
+sed "s/\.\/\.\./github.com\/jason-johnson\/tf-azure-keyvault?ref=$tag/" examples/example.tf > examples/generated.txt
 terraform-docs .
